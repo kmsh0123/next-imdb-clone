caae833ea76a5b1072989d74d00ea09f6b1bd40c
unknown <komauk1998@gmail.com>
2023-12-17 11:17:03 +0630

    finished

--- src/app/detail/[id]/page.jsx
+++ src/app/detail/[id]/page.jsx
@@ -18,10 +18,10 @@ const Detail = () => {
           placeholder="blur"
           blurDataURL="/spinner.svg"
         />
-       <div className='pt-5'>
+       <div className='pt-5 space-y-5'>
        <h1 className='md:ml-10 md:mx-0 text-2xl font-semibold mx-5 lg:text-start text-center'>{data?.title}</h1>
         <p className='md:ml-10 md:mx-0 mx-5 lg:text-start text-center'>{data?.overview}</p>
-        <div className='flex justify-between items-center my-5 md:ml-10 md:mx-0'>
+        <div className='flex justify-between items-center my-5 md:ml-10 mx-5'>
           <p className='font-black text-violet-800'>Vote : {data?.vote_count}</p>
           <p className='font-extrabold text-violet-800'>Rating : {data?.vote_average.toFixed(1)}</p>
        </div>
